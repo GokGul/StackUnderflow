400e1e4a50c73299cea3261ed4c88e0d691db737
GokGuu <gokhan_g32@yahoo.com>
2016-01-25 17:53:07 +0100

    Created DAL and DBContext file

---
+++ StackUnderflow/DAL/StackUnderflowContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using StackUnderflow.Models;
+
+namespace StackUnderflow.DAL
+{
+    public class StackUnderflowContext : DbContext
+    {
+        public StackUnderflowContext() : base("StackUnderflowContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Badge> Badges { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<QuestionMarked> QuestionsMarked { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<QuestionEdit> QuestionsEdited { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<AnswerEdit> AnswersEdited { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
