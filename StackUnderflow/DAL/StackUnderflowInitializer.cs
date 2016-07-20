95ad1c61ca1d0c9f87f87256b9cc13ff023c6a67
GokGuu <gokhan_g32@yahoo.com>
2016-07-20 23:21:55 +0200

    Start in initializer.

--- StackUnderflow/DAL/StackUnderflowInitializer.cs
+++ StackUnderflow/DAL/StackUnderflowInitializer.cs
@@ -10,7 +10,25 @@ namespace StackUnderflow.DAL
     {
         protected override void Seed(StackUnderflowContext context)
         {
-            
+            var users = new List<User>
+            {
+                new User { Username="Hans", Email="hans@hotmail.com", Password="123456789" },
+                new User { Username="Hamed", Email="hamed@hotmail.com", Password="123456789" },
+                new User { Username="Abdoel", Email="abdoel@hotmail.com", Password="123456789" }
+            };
+            users.ForEach(s => context.Users.Add(s));
+            context.SaveChanges();
+
+            var badges = new List<Badge>
+            {
+                new Badge { Name="Beginner Java", Level="Bronze", Requirement="Answer 10 Java related questions.", points=10},
+                new Badge { Name="Intermediate Java", Level="Silver", Requirement="Answer 30 Java related questions.", points=20},
+                new Badge { Name="Expert Java", Level="Gold", Requirement="Answer 50 Java related questions.", points=30}
+            };
+            badges.ForEach(s => context.Badges.Add(s));
+            context.SaveChanges();
+
+
         }
     }
 }
